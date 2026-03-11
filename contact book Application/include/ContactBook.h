d553322e60d86905f88abe338d74d25824e9b04b
Mohamed Ashraf <mo2003ashraf17@gmail.com>
2026-03-11 13:11:10 +0200

    Add files via upload

---
+++ contact book Application/include/ContactBook.h
@@ -0,0 +1,87 @@
+#ifndef CONTACTBOOK_H
+#define CONTACTBOOK_H
+#include <iostream>
+#include <contact.h>
+using namespace std;
+class ContactBook
+{
+private:
+    int counter=0;
+    contact contacts[1000];
+public:
+    void addContact()
+    {
+        contact c;
+        c.information();
+        contacts[counter]=c;
+        counter++;
+    }
+    void deleteContact()
+    {
+        cout<<"Please Enter ID For Contact To Delete :"<<endl;
+        int x;
+        cin>>x;
+        int i;
+        for(i=0; i<counter; i++)
+        {
+            if(x==contacts[i].getId())
+            {
+                contacts[i]=contacts[counter-1];
+                break;
+            }
+        }
+        if(i==counter)
+        {
+            cout<<"The Contact Not Found"<<endl;
+        }
+        counter--;
+    }
+    void editContact()
+    {
+        cout<<"Please Enter ID For Contact To Edit :"<<endl;
+        int x;
+        cin>>x;
+        int i;
+        for(i=0; i<counter; i++)
+        {
+            if(x==contacts[i].getId())
+            {
+                contacts[i].information();
+                break;
+            }
+        }
+        if(i==counter)
+        {
+            cout<<"The Contact Not Found"<<endl;
+        }
+    }
+    void search()
+    {
+        cout<<"Please Enter ID For Contact To Edit :"<<endl;
+        int x;
+        cin>>x;
+        int i;
+        for(i=0; i<counter; i++)
+        {
+            if(x==contacts[i].getId())
+            {
+                contacts[i].print();
+                break;
+            }
+        }
+        if(i==counter)
+        {
+            cout<<"The Contact Not Found"<<endl;
+        }
+    }
+    void printAll()
+    {
+        for(int i=0;i<counter;i++)
+        {
+            contacts[i].print();
+        }
+    }
+
+};
+
+#endif // CONTACTBOOK_H
